876a00a7fcfc6b89d361e104973120c3a0b5bb70
Francisco Yip <francisco.yip@gmail.com>
2016-07-18 14:03:33 -0700

    first functional commit

---
+++ scripts/app.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){
+
+    var LocalWeather = function(){
+
+      var locationResponse = null;
+      var localWeather = null;
+      var displayUnit = 'F';
+      var elementCache = null;
+
+      function getCurrentLocation(){
+        //return a thenable Promise
+        var url = 'http://ip-api.com/json';
+        return $.ajax({
+          url: url,
+          success: function(data){
+            locationResponse = data;
+            console.log(data);
+          }
+        });
+      }
+
+      function getCurrentWeather(){
+        var url = 'http://api.openweathermap.org/data/2.5/weather';
+        var data = {
+          //bad idea to embed API Key here
+          appid: ''
+        };
+        if(locationResponse !== null){
+          data['q'] = [locationResponse['city'],locationResponse['region'],locationResponse['countryCode']].join(',');
+          return $.ajax({
+            url: url,
+            data: data,
+            success: function(data) {
+              localWeather = data;
+              console.log(data);
+            }
+          });
+        }
+      };
+
+      function renderUI(){
+        if(locationResponse !== null){
+          if(elementCache === null){
+            cacheElements();
+          }
+          elementCache['$location'].html( locationResponse['city']+', '+locationResponse['region'] );
+          var degree = localWeather['main']['temp'];
+          degree = (displayUnit === 'F'? (degree * 9/5) - 457.87 : degree - 273.15).toFixed(2);
+          elementCache['$degree'].html(degree+'&deg;' + displayUnit);
+          var iconCode = localWeather.weather[0].icon;
+          var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+
+          elementCache['$icon'].empty().append( $('<img>').attr({
+            'src':iconUrl
+          }).css({width: '100px'}) );
+          elementCache['$description'].html(localWeather['weather'][0]['main']);
+          var ts = new Date(0);
+          ts.setUTCSeconds( parseInt( localWeather['dt'] ) )
+          elementCache['$timestamp'].html('Last Updated on <br/>' + ts.toString('MMMM dS, yyyy HH:mm'));
+        }
+      }
+
+      function cacheElements(){
+        var $weather = $('.weather')
+        elementCache = {
+          $location:$('#location'),
+          $degree:$weather.find('.degree'),
+          $unit:$weather.find('.unit'),
+          $icon:$weather.find('.icon'),
+          $description:$weather.find('.description'),
+          $timestamp:$weather.find('.timestamp')
+        }
+      }
+
+      return {
+        getCurrentLocation: getCurrentLocation,
+        getCurrentWeather: getCurrentWeather,
+        renderUI: renderUI
+      };
+    }();
+
+    LocalWeather.getCurrentLocation()
+    .then(LocalWeather.getCurrentWeather)
+    .then(LocalWeather.renderUI);
+});
